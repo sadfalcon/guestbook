4cc6275793d958f5e77717119db2442dae337f6d
talgar <otalgarbek@gmail.com>
2020-08-01 11:57:30 +0600

    файл admin.py настроен

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Card
 
-# Register your models here.
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    list_display = ('pk', 'name', 'mail', 'text', 'created_at', 'updated_at')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Card, CardAdmin)
