18280f37d40d66218e7ed92204bbc2773ee55999
talgar <otalgarbek@gmail.com>
2020-08-01 13:02:21 +0600

    добавлен базовый шаблон и шаблон главной страницы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.utils.timezone import make_naive
 
+from webapp.models import Card
+#from webapp.forms import CardForm
 # Create your views here.
+
+def index_view(request):
+    data = Card.objects.all().order_by('-created_at')
+
+    return render(request, 'index.html', context={
+        'cards': data
+    })
