82bd032301b3e5bcdbfd6d489b86605457fe1d02
talgar <otalgarbek@gmail.com>
2020-08-01 12:31:05 +0600

    добавлен дамп данных

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -5,7 +5,7 @@ from .models import Card
 
 class CardAdmin(admin.ModelAdmin):
     list_filter = ('status',)
-    list_display = ('pk', 'name', 'mail', 'text', 'created_at', 'updated_at')
+    list_display = ('pk', 'name', 'mail', 'text', 'created_at', 'updated_at', 'status')
     list_display_links = ('pk', 'name')
     search_fields = ('name',)
 
